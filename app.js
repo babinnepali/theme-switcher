4e6f50034dd132223554b098fcc715d861377dff
Babin Singh Nepali <babinnepalibtl@gmail.com>
2020-11-06 10:09:05 +0545

    local storage

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 console.log("Hello, this my first dark theme integration.");
 
+if(localStorage.getItem('theme')==="dark"){
+    document.body.classList.toggle(localStorage.getItem('theme'));
+}
 const themeSwitcher = document.getElementById('theme__switcher');
     themeSwitcher.addEventListener('click', function(){
         document.body.classList.toggle('dark');
+        localStorage.setItem('theme', document.body.classList);
     })
