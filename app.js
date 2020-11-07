eff69784bd97f3fac89dba4fd96b7a25b2f95a1d
Babin Singh Nepali <babinnepalibtl@gmail.com>
2020-11-07 20:35:28 +0545

    slide switcher(checkbox)

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 console.log("Hello, this my first dark theme integration.");
 
+//storing the latest [theme value] in the Local Storage
 if(localStorage.getItem('theme')==="dark"){
     document.body.classList.toggle(localStorage.getItem('theme'));
 }
-const themeSwitcher = document.getElementById('theme__switcher');
+
+
+const themeSwitcher = document.getElementById('toggle__fill');
     themeSwitcher.addEventListener('click', function(){
         document.body.classList.toggle('dark');
         localStorage.setItem('theme', document.body.classList);
